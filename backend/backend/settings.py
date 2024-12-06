31f95635bb59ab1cf82f26230ba73c05181297fb
mohabmagdy009 <mohab.magdy@hotmail.com>
2024-12-06 21:40:38 +0200

    Set up inventory app
    - Created `inventory` app containing
    - Updated the .env to include email and password
    - Updated `settings.py` with necessary email configurations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
     # Your apps
     'utils',
+    'inventory'
 ]
 # endregion
 
@@ -254,3 +255,12 @@ SIMPLE_JWT = {
     'UPDATE_LAST_LOGIN': False,
 }
 # endregion
+
+# region Email Configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL')  # Your email address
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  # Your email password
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL')
